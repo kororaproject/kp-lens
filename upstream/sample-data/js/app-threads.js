8e9568d80b82db1bc18df17c63be48df26383a45
Michael Donnelly <mike@donnellyonline.com>
2014-09-18 20:56:48 +1000

    Moved lens specific javascript into an angular module named kp-lens Updated sample apps to use kp-lens module

--- upstream/sample-data/js/app-threads.js
+++ upstream/sample-data/js/app-threads.js
@@ -1,35 +1,4 @@
-var app = angular.module('lens-app', []);
-
-// call the python-webkit bridge
-app.run(function($rootScope) {
-  $rootScope.emitPYTHON = function() {
-    var _args = Array.prototype.slice.call(arguments);
-
-    if( _args.length > 0 ) {
-      var _command = {
-        name: _args.shift(),
-        args: _args
-      }
-
-      /* update document title */
-      var _title = document.title;
-      document.title = '_BR::' + angular.toJson(_command, false);
-      document.title = _title;
-    }
-  };
-
-  $rootScope.safeApply = function(fn) {
-    var phase = this.$root.$$phase;
-    if(phase == '$apply' || phase == '$digest') {
-      if(fn && (typeof(fn) === 'function')) {
-        fn();
-      }
-    }
-    else {
-      this.$apply(fn);
-    }
-  };
-});
+var app = angular.module('lens-app', ['kp-lens']);
 
 function AppCtrl($scope) {
   $scope.hostname = 'unknown';
--- upstream/sample-data/js/app.js
+++ upstream/sample-data/js/app.js
@@ -1,35 +1,8 @@
-var app = angular.module('lens-app', []);
+var app = angular.module('lens-app', ['kp-lens']);
 
 // call the python-webkit bridge
-app.run(function($rootScope) {
-  $rootScope.emitPYTHON = function() {
-    var _args = Array.prototype.slice.call(arguments);
-
-    if( _args.length > 0 ) {
-      var _command = {
-        name: _args.shift(),
-        args: _args
-      }
-
-      /* update document title */
-      var _title = document.title;
-      document.title = '_BR::' + angular.toJson(_command, false);
-      document.title = _title;
-    }
-  };
-
-  $rootScope.safeApply = function(fn) {
-    var phase = this.$root.$$phase;
-    if(phase == '$apply' || phase == '$digest') {
-      if(fn && (typeof(fn) === 'function')) {
-        fn();
-      }
-    }
-    else {
-      this.$apply(fn);
-    }
-  };
-});
+//app.run(function($rootScope) {
+//});
 
 function AppCtrl($scope) {
   $scope.hostname = 'unknown';

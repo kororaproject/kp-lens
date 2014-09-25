af68b141117d5fd9ddcb85ec8e132323cb1795c4
firnsy <firnsy@gmail.com>
2014-09-25 13:47:30 +1000

    updated: added ui-bootstrap to core.

--- upstream/build-bundles.sh
+++ upstream/build-bundles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat lens-data/js/{jquery,angular,angular-route,angular-animate,bootstrap}.js lens-data/js/{lens-bridge,lens-ui}.js > lens-data/js/lens.js
-cat lens-data/js/{jquery,angular,angular-route,angular-animate,bootstrap}.min.js lens-data/js/{lens-bridge,lens-ui}.js > lens-data/js/lens.min.js
+cat lens-data/js/{jquery,angular,angular-route,angular-animate,bootstrap,ui-bootstrap}.js lens-data/js/{lens-bridge,lens-ui}.js > lens-data/js/lens.js
+cat lens-data/js/{jquery,angular,angular-route,angular-animate,bootstrap,ui-bootstrap}.min.js lens-data/js/{lens-bridge,lens-ui}.js > lens-data/js/lens.min.js
